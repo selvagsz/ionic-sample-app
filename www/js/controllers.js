f87e0b44036b756a53ca73c153c689ed7351142d
selvagsz <selvaganeshbsg@gmail.com>
2016-08-02 13:09:03 +0530

    RZP pay

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,37 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicPlatform) {
+  var options = {
+    description: 'Credits towards consultation',
+    image: 'https://i.imgur.com/3g7nmJC.png',
+    currency: 'INR',
+    key: 'rzp_test_1DP5mmOlF5G5ag',
+    amount: '5000',
+    name: 'foo',
+    prefill: {
+      email: 'pranav@razorpay.com',
+      contact: '8879524924',
+      name: 'Pranav Gupta'
+    },
+    theme: {
+      color: '#F37254'
+    }
+  };
+
+  var successCallback = function(payment_id) {
+    alert('payment_id: ' + payment_id);
+  };
+
+  var cancelCallback = function(error) {
+    alert(error.description + ' (Error ' + error.code + ')');
+  };
+
+  $ionicPlatform.ready(function(){
+    $scope.pay = function() {
+      RazorpayCheckout.open(options, successCallback, cancelCallback);
+    }
+  });
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
